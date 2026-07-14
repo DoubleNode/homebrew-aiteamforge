f2b34a907f0057a0cf98d992cb568ad9089532a7
Darren Ehlers <me@darrenehlers.com>
2026-07-14 16:20:57 -0500

    chore: Release AITeamForge 0.17.8

--- Formula/aiteamforge.rb
+++ Formula/aiteamforge.rb
@@ -2,7 +2,7 @@ class Aiteamforge < Formula
   desc "AI-powered multi-team development infrastructure"
   homepage "https://github.com/DoubleNode/homebrew-aiteamforge"
   url "https://github.com/DoubleNode/homebrew-aiteamforge.git",
-      tag: "v0.17.7"
+      tag: "v0.17.8"
   license "MIT"
 
   head "https://github.com/DoubleNode/homebrew-aiteamforge.git", branch: "main"
